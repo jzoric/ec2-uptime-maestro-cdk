9694ccdb04503ebd8f1d2e7bbd2e0c563f08fc18
Jovica Zoric <11492884+jzoric@users.noreply.github.com>
2025-02-23 12:52:45 +0100

    Use Node.js modules
    Closes: #4

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,10 +9,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   packageManager: javascript.NodePackageManager.NPM,
   projenrcTs: true,
   repositoryUrl: 'https://github.com/jzoric/ec2-uptime-maestro-cdk.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.addBundledDeps("sync-request");
 project.synth();
